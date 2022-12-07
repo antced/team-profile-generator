d00ecb840b9766fc7c987cf117bebc19d57d4b5f
Anthony Cedrone <antcedrone95@gmail.com>
2022-12-06 19:36:44 -0500

    started making html and css

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
+const Employee = require("./lib/Employee");
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
@@ -8,11 +9,11 @@ const engineerArr = [];
 const internArr = [];
 // prompt for manager
 const makeManager = () => {
-// GIVEN a command-line application that accepts user input
-// WHEN I am prompted for my team members and their information
-// WHEN I start the application
-// THEN I am prompted to enter the team manager’s name, employee ID, email address, and office number
-// WHEN I enter the team manager’s name, employee ID, email address, and office number
+    // GIVEN a command-line application that accepts user input
+    // WHEN I am prompted for my team members and their information
+    // WHEN I start the application
+    // THEN I am prompted to enter the team manager’s name, employee ID, email address, and office number
+    // WHEN I enter the team manager’s name, employee ID, email address, and office number
     inquirer
         .prompt([
             {
@@ -35,7 +36,7 @@ const makeManager = () => {
                 message: "What is the team manager's office number?",
                 name: "office",
             },
-// THEN I am presented with a menu with the option to add an engineer or an intern or to finish building my team
+            // THEN I am presented with a menu with the option to add an engineer or an intern or to finish building my team
             {
                 type: "list",
                 message: "What kind of employee would you like to add?",
@@ -44,20 +45,20 @@ const makeManager = () => {
             }
         ])
         .then((response) => {
-            const manager = new Manager(response.office);
-            managerArr.push(manager);
+            const manager = new Manager(response.name, response.id, response.email, response.office);
+            managerArr.push([manager]);
             makeEmployee(response.employee);
         });
 };
 // prompt for other employees
 const makeEmployee = (employeeType) => {
-// WHEN I decide to finish building my team
-// THEN I exit the application, and the HTML is generated
-    if (employeeType === "I am done adding employees") {
-        return
+    // WHEN I decide to finish building my team
+    // THEN I exit the application, and the HTML is generated
+    if (employeeType === "I am done adding employees.") {
+        makeHTML();
     } else if (employeeType === "Engineer") {
-// WHEN I select the engineer option
-// THEN I am prompted to enter the engineer’s name, ID, email, and GitHub username, and I am taken back to the menu
+        // WHEN I select the engineer option
+        // THEN I am prompted to enter the engineer’s name, ID, email, and GitHub username, and I am taken back to the menu
         inquirer
             .prompt([
                 {
@@ -88,13 +89,13 @@ const makeEmployee = (employeeType) => {
                 }
             ])
             .then((response) => {
-                const engineer = new Engineer(response.github);
-                engineerArr.push(engineer);
+                const engineer = new Engineer(response.name, response.id, response.email, response.github);
+                engineerArr.push([engineer]);
                 makeEmployee(response.employee);
             });
     } else if (employeeType === "Intern") {
-// WHEN I select the intern option
-// THEN I am prompted to enter the intern’s name, ID, email, and school, and I am taken back to the menu
+        // WHEN I select the intern option
+        // THEN I am prompted to enter the intern’s name, ID, email, and school, and I am taken back to the menu
         inquirer
             .prompt([
                 {
@@ -125,30 +126,105 @@ const makeEmployee = (employeeType) => {
                 }
             ])
             .then((response) => {
-                const intern = new Intern(response.school);
-                internArr.push(intern);
+                const intern = new Intern(response.name, response.id, response.email, response.school);
+                internArr.push([intern]);
                 makeEmployee(response.employee);
             });
     }
 };
 
-const managerCard = `
-<div class="card p-0 m-2 shadow" style="width: 18rem;">
-<div class="card-header bg-primary text-white">
-<h5 class="card-title">${employee.name}</h5>
-<h6 class="card-title">${employee.role}</h6>
-</div>
-<div class="card-body bg-light">
-<ul class="list-group list-group-flush border">
-<li class="list-group-item">ID: ${employee.id}</li>
-<li class="list-group-item">Email: ${employee.email}</li>
-<li class="list-group-item">Office Number: ${employee.office}</li>
-</ul>
-</div>
-</div>`
-
 const makeHTML = () => {
-    console.log(managerArr)
+    const boilerPlate = `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <title>My Team</title>
+    </head>
+    <body>
+    <header>
+    <h1 class="bg-danger p-5 text-center text-white">My Team</h1>
+    </header>
+    <section class="container">
+    <div class="d-flex flex-wrap justify-content-center">`
+    // write the boilerplate html 
+    fs.writeFile('index.html', boilerPlate, (err) =>
+        err ? console.error(err) : console.log('Created index.html'));
+
+    const managerCard = `
+    <div class="card p-0 m-2 shadow" style="width: 18rem;">
+    <div class="card-header bg-primary text-white">
+    <h5 class="card-title">${managerArr[0][0].name}</h5>
+    <h6 class="card-title"><i class="fa-solid fa-mug-hot me-2"></i>${managerArr[0][0].constructor.name}</h6>
+    </div>
+    <div class="card-body bg-light">
+    <ul class="list-group list-group-flush border">
+    <li class="list-group-item">ID: ${managerArr[0][0].id}</li>
+    <li class="list-group-item">Email: <a href="mailto: ${managerArr[0][0].email}">${managerArr[0][0].email}</a></li>
+    <li class="list-group-item">Office Number: ${managerArr[0][0].office}</li>
+    </ul>
+    </div>
+    </div>`
+    fs.appendFile('./index.html', managerCard, (err) =>
+        err ? console.error(err) : console.log('Created manager card'));
+
+    if (engineerArr.length > 0) {
+        for (let i = 0; i < engineerArr.length; i++) {
+            const engineerCard = `
+            <div class="card p-0 m-2 shadow" style="width: 18rem;">
+            <div class="card-header bg-primary text-white">
+            <h5 class="card-title">${engineerArr[i][0].name}</h5>
+            <h6 class="card-title"><i class="fa-solid fa-glasses me-2"></i>${engineerArr[i][0].constructor.name}</h6>
+            </div>
+            <div class="card-body bg-light">
+            <ul class="list-group list-group-flush border">
+            <li class="list-group-item">ID: ${engineerArr[i][0].id}</li>
+            <li class="list-group-item">Email: <a href="mailto: ${engineerArr[i][0].email}">${engineerArr[i][0].email}</a></li>
+            <li class="list-group-item">GitHub: <a href="https://github.com/${engineerArr[i][0].github}" target="_blank">${engineerArr[i][0].github}</a></li>
+            </ul>
+            </div>
+            </div>`
+            fs.appendFile('./index.html', engineerCard, (err) =>
+                err ? console.error(err) : console.log('Created engineer card'));
+        }
+    }
+
+    if (internArr.length > 0) {
+        for (let i = 0; i < internArr.length; i++) {
+            const internCard = `
+            <div class="card p-0 m-2 shadow" style="width: 18rem;">
+            <div class="card-header bg-primary text-white">
+            <h5 class="card-title">${internArr[i][0].name}</h5>
+            <h6 class="card-title"><i class="fa-solid fa-user-graduate me-2"></i>${internArr[i][0].constructor.name}</h6>
+            </div>
+            <div class="card-body bg-light">
+            <ul class="list-group list-group-flush border">
+            <li class="list-group-item">ID: ${internArr[i][0].id}</li>
+            <li class="list-group-item">Email: <a href="mailto: ${internArr[i][0].email}">${internArr[i][0].email}</a></li>
+            <li class="list-group-item">School: ${internArr[i][0].school}</li>
+            </ul>
+            </div>
+            </div>`
+            fs.appendFile('./index.html', internCard, (err) =>
+                err ? console.error(err) : console.log('Created intern card'));
+        }
+    }
+    
+    const htmlEnd = `
+    </div>
+    </section>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4"
+    crossorigin="anonymous"></script>
+    </body>
+    </html>`
+    fs.appendFile('./index.html', htmlEnd, (err) =>
+        err ? console.error(err) : console.log('Created end of html'));
 }
 
 // THEN an HTML file is generated that displays a nicely formatted team roster based on user input
@@ -158,6 +234,5 @@ const makeHTML = () => {
 // THEN that GitHub profile opens in a new tab
 
 makeManager();
-makeHTML()
 
 
